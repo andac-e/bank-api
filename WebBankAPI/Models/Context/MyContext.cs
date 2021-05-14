d0e821a493f5198de9ded905a1256d02cb37d1a5
andac-e <65905654+andac-e@users.noreply.github.com>
2021-05-14 09:02:49 +0300

    Add project files.

---
+++ WebBankAPI/Models/Context/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebBankAPI.Models.Entities;
+using WebBankAPI.Models.Init;
+
+namespace WebBankAPI.Models.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            Database.SetInitializer(new MyInit());
+        }
+
+        public DbSet<CardInfo> Cards { get; set; }
+
+    }
+}
---
+++ WebBankAPI/Models/Init/MyInit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebBankAPI.Models.Context;
+using WebBankAPI.Models.Entities;
+
+namespace WebBankAPI.Models.Init
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInfo ci = new CardInfo();
+            ci.CardUserName = "Andaç Erdoğmuş";
+            ci.CardNumber = "1111 1111 1111 1111";
+            ci.CardExpiryYear = 2022;
+            ci.CardExpiryMonth = 12;
+            ci.SecurityNumber = "999";
+            ci.Limit = 40000;
+            ci.Balance = 40000;
+
+            context.Cards.Add(ci);
+            context.SaveChanges();
+        }
+    }
+}
